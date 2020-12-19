c7759e5a8582cbb6c9091c95f44885a411292328
fatute11 <fatimazahra.elhasbi@gmail.com>
2020-12-19 17:24:20 +0100

    initialisation project deno

---
+++ interfaces/UserInterfaces.ts
@@ -0,0 +1,22 @@
+import { roleTypes } from '../types/roleTypes.ts';
+import { userUpdateTypes } from '../types/userUpdateTypes.ts';
+
+export default interface UserInterfaces {
+
+    _id: { $oid: string }|null|string;
+
+    email: string;
+    password: string;
+    lastname: string;
+    firstname: string;
+    phoneNumber ? : string;
+
+    dateNaiss: Date;
+    role: roleTypes;
+
+    getAge(): Number;
+    fullName(): string;
+    insert(): Promise < void > ;
+    update(update:userUpdateTypes): Promise < any > ;
+    delete(): Promise < any > ;
+}
---
+++ server.ts
@@ -0,0 +1,21 @@
+import * as expressive from "https://raw.githubusercontent.com/NMathar/deno-express/master/mod.ts";
+import { config } from './config/config.ts';
+import { UserModels } from './Models/UserModels.ts';
+
+const port = 8001;
+const app = new expressive.App();
+// route with dynamic parameter
+app.get("/", async(req, res) => {
+    await res.json([
+        { id: 2, name: "Jim Doe", phone: "12425323" },
+    ]);
+});
+
+let user = new UserModels('kjkj','kjkj','kjkj','kjkj','kjkj',"1993-11-22");
+user.insert()
+console.log(user);
+
+
+const server = await app.listen(port);
+// deno run --allow-net --allow-read --unstable server.ts
+console.log("app listening on port " + server.port);
