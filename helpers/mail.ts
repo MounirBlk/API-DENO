939e622503d7bf521a15cd394aa986ec68f71ea4
fatute11 <fatimazahra.elhasbi@gmail.com>
2021-01-22 14:40:53 +0100

    envoie de mail

--- helpers/mail.ts
+++ helpers/mail.ts
@@ -1,22 +1,24 @@
 import { SmtpClient } from "https://deno.land/x/smtp/mod.ts";
 import "https://deno.land/x/dotenv/load.ts";
+import {exist} from "../middlewares/index.ts";
+import { getRandomFilename } from "https://deno.land/x/oak@v6.4.1/util.ts";
 
-const client = new SmtpClient();
-const { SEND_EMAIL, PWD, RECV_EMAIL } = Deno.env.toObject();
 
-const connectConfig: any = {
+const sendMail = async (email:string): Promise <void>=>{ 
+const client = new SmtpClient(); 
+  await client.connectTLS({
     hostname: "smtp.gmail.com",
     port: 465,
-    username: SEND_EMAIL,
-    password: PWD,
-  };
-  await client.connectTLS(connectConfig);
-
+    username: Deno.env.get("SEND_EMAIL"),
+    password: Deno.env.get("SEND_PWD") ,
+  });
   await client.send({
-    from: SEND_EMAIL,
-    to: RECV_EMAIL,
+    from: exist(<string>Deno.env.get("SEND_EMAIL")) ? <string>Deno.env.get("SEND_EMAIL"): "exemple@gmail.com",
+    to: email,
     subject: "Welcome!",
     content: "Hi from Vuelancer!",
   });
-
   await client.close();
+}
+
+sendMail('email@test.com');
